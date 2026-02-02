e72c8788f1b958068ad890f674977fb7d7497f88
elliot14A@nixos <akshithkatkuri14@gmail.com>
2026-02-01 20:30:38 -0600

    security: disable greetd auto-login for proper authentication
    - Remove initial_session configuration that bypassed login prompt
    - Keep default_session with tuigreet for proper credential verification
    - System will now require username/password on boot instead of auto-login
    - Fixes security vulnerability where system logged in automatically

--- modules/niri.nix
+++ modules/niri.nix
@@ -53,11 +53,6 @@ in
           command = "${pkgs.tuigreet}/bin/tuigreet --cmd niri-session";
           user = "greeter";
         };
-
-        initial_session = {
-          command = "niri-session";
-          user = "kiwi";
-        };
       };
     };
   };
